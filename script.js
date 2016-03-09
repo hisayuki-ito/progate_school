19480b9ad086e4b81bf340ba28fbee1b2021f211
伊藤久之 <itohisa0311@gmail.com>
2016-03-09 21:47:44 +0900

    test

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 $(function(){
 
+// 問題２　もっと詳しくボタン
+	  $('#read-more-btn').click(function(){
+	  	if($(this).hasClass('open')){
+	      $('#intro').fadeOut();
+	      $('.read-more').removeClass('open');
+	      $(this).text('もっと詳しく');
+	  	}else{
+	      $('#intro').fadeIn();
+	      $(this).addClass('open');
+	      $(this).text('閉じる');
+	  	}
+	  });
 
+// 　　問題３ 学習言語ホバー
+	　$('.language-box').hover(
+	    function() {
+	      $(this).find('.lang-sentence').addClass('text-active');
+	      
+	    },
+	    function() {
+	      $(this).find('.lang-sentence').removeClass('text-active');
+	      
+	    }
+	  );
 
+// 問６　コラム画像ホバー
+      $('.column-image').hover(
+	    function() {
+	      $(this).css('zoom','1.2');
+	      
+	    },
+	    function() {
+	      $(this).find('.lang-sentence').removeClass('text-active');
+	      
+	    }
+	  );
 
 	
 });
